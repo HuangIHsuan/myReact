21dd271bec63ebb1073a756a8a5d4f7213607666
sharon12307 <sharon12307@gmail.com>
2024-12-03 09:30:40 +0800

    1203_refresh

--- my-react-app1/src/App.jsx
+++ my-react-app1/src/App.jsx
@@ -7,9 +7,9 @@ function App() {
 
   useEffect(() => {
     (async () => {
-      const data = await axios.get('./F-C0032-001.json');
+      const data = await axios.get('../public/json/F-C0032-001.json');
       const { location } = data.data.cwaopendata.dataset;
-      console.log(location);
+      console.log(data);
     })
   }, [])
 
